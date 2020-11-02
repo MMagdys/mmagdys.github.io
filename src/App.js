ffb8262f1a1cd9620ca5ffee219578c96a5fd624
mmagdys <muhammad.magdy50@gmail.com>
2020-11-02 08:38:04 +0200

    deployment ready modifications

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import {BrowserRouter} from 'react-router-dom';
-
+import { createBrowserHistory } from "history";
 
 import Main from './layouts/Main';
+const hist = createBrowserHistory();
+
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL} history={hist}>
       <Main />
     </BrowserRouter>
   );
